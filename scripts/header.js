12811d271ef816566d24d1dd28828bde60aac2f9
C-ArenA <carlostata.ca@gmail.com>
2021-03-14 01:29:41 -0400

    Header totally finished and hero section also finished. Imm working on the services section now

--- scripts/header.js
+++ scripts/header.js
@@ -4,7 +4,7 @@
     let navList = document.querySelector(".hamburger-list");
     let hamburger = document.getElementById("hamburger");
     let items = navList.children[0].children;
-    
+    let header = document.getElementsByTagName("header")[0];
     console.log(items);
     
     let showResponsiveNav = () => {
@@ -27,5 +27,17 @@
         console.log(item);
         item.addEventListener("click", showResponsiveNav);
     }
+
+    window.addEventListener("scroll", ()=>{
+        if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
+            header.style["height"] = "5vw";
+            navList.style["top"] = "80px";
+            console.log("Así es")
+        } else {
+            header.style["height"] = "20vw";
+            navList.style["top"] = "19vw";
+            console.log("No es");
+        }
+    })
     console.log("Hola, Finalizando script del header");
 })()
